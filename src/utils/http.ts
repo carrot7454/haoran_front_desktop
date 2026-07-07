0d849c4c5e03d82700cc90a2f74aac385b65cad6
carrot7454 <carrot.008@163.com>
2026-07-07 20:12:44 +0800

    >>>

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -5,6 +5,11 @@
 import { request } from '@@/plugin-request';
 
 const http = async (url: string, method: 'POST' | 'GET', data?: any) => {
+  const user = localStorage.getItem('user');
+  if (!user) {
+    location.replace('/login');
+    return null;
+  }
   return await request('http://127.0.0.1:3000' + url, {
     method,
     data,
