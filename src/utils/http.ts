2634ade84ddb75426d09a029c04adda3af714b3f
carrot7454 <carrot.008@163.com>
2026-07-07 20:38:27 +0800

    >>>>>

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -3,14 +3,18 @@
  * @LastEditors: Luoxiangyu
  */
 import { request } from '@@/plugin-request';
+import { history } from 'umi';
+
+// const baseUrl = 'http://127.0.0.1:3000';
+const baseUrl = 'https://zhishiliu.top/api';
 
 const http = async (url: string, method: 'POST' | 'GET', data?: any) => {
   const user = localStorage.getItem('user');
   if (!user) {
-    location.replace('/login');
+    history.replace('/login');
     return null;
   }
-  return await request('http://127.0.0.1:3000' + url, {
+  return await request(baseUrl + url, {
     method,
     data,
     headers: {
